2d5c98d012f72fa25e634cbd7f681ae777ab94a8
Suleimmen <148604011+Jemenei@users.noreply.github.com>
2024-01-24 09:28:12 +0600

    Add files via upload

---
+++ Lab PP2/lab 1/Data_types.py
@@ -0,0 +1,23 @@
+#ex 1
+x = 5
+print(type(x))
+#Answer: int
+
+#ex 2
+x = "Hello World"
+print(type(x))
+#Answer: string
+
+#ex 3
+x = 20.5
+print(type(x))
+#Answer: float
+
+#ex 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Answer: list
+
+#ex 5
+x = ["apple", "banana", "cherry"]
+print(type(x))
