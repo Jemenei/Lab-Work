53505250b67167ae27055a75e112904e67ac1c7a
Suleimmen <148604011+Jemenei@users.noreply.github.com>
2024-01-24 10:35:44 +0600

    Add files via upload

---
+++ Lab PP2/lab 2/Booleans.py
@@ -0,0 +1,19 @@
+#ex 1
+print(10 > 9)
+Answer: True
+
+#ex 2
+print(10 == 9)
+Answer: False
+
+#ex 3
+print(10 < 9)
+Answer: False
+
+#ex 4
+print(bool("abc"))
+Answer: True
+
+#ex 4
+print(bool(0))
+Answer: False
---
+++ Lab PP2/lab 2/Operators.py
@@ -0,0 +1,19 @@
+#ex 1
+print(10 * 5)
+
+
+#ex 2
+print(10 / 2)
+
+#ex 3
+fruits = ["Apple", "Banana"]
+if "Apple" in fruits:
+    print("Yes, apple is fruit!")
+
+#ex 4
+if 5 != 10:
+    print("5 and 10 is not equal")
+
+#ex 5
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
