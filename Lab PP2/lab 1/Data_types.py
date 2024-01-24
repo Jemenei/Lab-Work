4b1d15cccfc265e17e9f34b4fe56ad2456d339fa
Suleimmen <148604011+Jemenei@users.noreply.github.com>
2024-01-24 12:11:08 +0600

    Add files via upload

---
+++ Lab PP2/lab 1/Data_types.py
@@ -0,0 +1,34 @@
+#ex 1
+x = 5
+print(type(x))
+Answer: int
+
+#ex 2
+x = "Hello World"
+print(type(x))
+Answer: str
+
+#ex 3
+x = 20.5
+print(type(x))
+Answer: float
+
+#ex 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+Answer: list
+
+#ex 5
+x = ["apple", "banana", "cherry"]
+print(type(x))
+Answer: tuple
+
+#ex 6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+Answer: dict
+
+#ex 7
+x = True
+print(type(x))
+Answer: bool
---
+++ Lab PP2/lab 1/Variables.py
@@ -0,0 +1,27 @@
+#ex 1 
+carname = "Volvo"
+
+#ex 2
+x = 50
+
+#ex 3
+x = 5
+y = 10
+print(x + y)
+
+#ex 4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#ex 5
+x, y, z = "Orange", "Banana", "Apple"
+
+#ex 6
+x = y = z = "Orange"
+
+#ex 7
+def myFunc():
+    global x
+    x = "fantastic"
